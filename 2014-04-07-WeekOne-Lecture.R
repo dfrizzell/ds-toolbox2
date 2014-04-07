4e4283a9317e825f59a9e8a2d7d25202d932cd85
dfrizzell <dfrizze@gmail.com>
2014-04-07 15:40:53 -0700

    Testing the commit. First commit

---
+++ 2014-04-07-WeekOne-Lecture.R
@@ -0,0 +1,81 @@
+
+#2014-04-07 - Week One Videos
+
+#Script for following video lectures 
+con <- url("http://www.jhsph.edu", "r")
+x <- readLines(con)
+head(x)
+
+getwd()
+
+
+
+# Question 1
+# Which of the following are courses in the Data Science Specialization? Select all that apply.
+
+# Data Science 101 
+# Getting and Cleaning Data - Yes
+# R programming - Yes
+# Machine Learning for Hackers
+
+# Question 2
+# Why are we using R for the course track? Select all that apply.
+
+# R has a nice IDE, Rstudio.- YES
+# Packages are easy to install and "play nicely" together. - YES
+# R is the only programming language data scientists use.
+# R allows object oriented programming.
+
+# Question 3
+# What are good ways to find answers to questions in this course track? Select all that apply.
+
+# Emailing the community TAs
+# Looking through R help files. - YES
+# Asking a skilled friend. - YES
+# Expecting every answer to be in a lecture slide
+
+# Question 4
+# What are characteristics of good questions on the message boards? Select all that apply.
+
+# Begs for help without providing information.
+# Provides the minimum amount of information necessary to communicate the problem. - YES
+# Is polite and courteous. - YES
+# Is an exact homework question.
+
+
+#Question 5
+# Which of the following packages provides machine learning functionality? Select all that apply
+
+# gbm - YES
+# knitr - 
+# slidify - 
+# pamr - YES
+
+
+
+# WEEK 2 Lectures
+
+# Command Line Interface - GitHub Bash for windows.
+# Path is your working directory
+# Root is the top directory
+# command flags arguments - standard approach to running a command
+# 
+# pwd - working dir --- clear will clear everything 
+
+# Installed Git Bash to do all commandline GIT work.
+
+
+# INTRO TO GIT
+# Git is a free and open source version control system - keeps and manages the intermediate files.
+# Git everything is stored locally on your PC.
+
+
+
+# Intro to GitHub and createing a REpo
+# GitHub is a web-based hosting service - allows users to push and pull local repos to and from remote ones
+
+# Git = Local; GitHub = Remote and 
+
+
+
+
